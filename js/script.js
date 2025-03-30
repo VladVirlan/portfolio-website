7bea96c0253d4459bf567f7107841b7ca7fec31c
Andrei-Vlad Pisaltu-Virlan <vladvirlan.dev@gmail.com>
2025-03-30 19:20:22 +0100

    Phase 1 Final Commit: Finalising the portfolio website

--- js/script.js
+++ js/script.js
@@ -5,3 +5,14 @@ function toggleMenu()
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+document.getElementById("mobile-nav-icon").addEventListener("click", toggleMenu);
+document.getElementById("mobile-home-link").addEventListener("click", toggleMenu);
+document.getElementById("mobile-about-link").addEventListener("click", toggleMenu);
+document.getElementById("mobile-experience-link").addEventListener("click", toggleMenu);
+document.getElementById("mobile-projects-link").addEventListener("click", toggleMenu);
+document.getElementById("mobile-contact-link").addEventListener("click", toggleMenu);
+document.getElementById("mobile-blog-link").addEventListener("click", toggleMenu);
+document.getElementById("mobile-portfolio-link").addEventListener("click", toggleMenu);
+document.getElementById("mobile-login-link").addEventListener("click", toggleMenu);
+document.getElementById("mobile-addpost-link").addEventListener("click", toggleMenu);
